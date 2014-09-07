bdcfb4a4df4346e7e05473a7579b1da40212bf9b
Doug Hellmann <doug@doughellmann.com>
2014-09-07 14:59:41 -0400

    Let oslotest manage the six.move setting for mox
    This instruction to set up a move in six for the mox module conflicts
    with the one in oslotest. We are trying to use mox3 everywhere, so
    remove this instruction and let oslotest handle it.
    
    Imported from the incubator change with the same ID.
    
    Change-Id: I59d5799283233f8411044ddb15c8abfc8850014c

--- oslo/db/openstack/common/__init__.py
+++ oslo/db/openstack/common/__init__.py
@@ -10,8 +10,3 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
-import six
-
-
-six.add_move(six.MovedModule('mox', 'mox', 'mox3.mox'))
