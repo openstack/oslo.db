9a510e85ce38b58f6de771de84a10db2bbe60c1c
Matthew Treinish <mtreinish@kortar.org>
2015-01-11 16:53:21 -0500

    Fix slowest test output after test run
    This commit fixes the output from pretty_tox.sh so that the testr
    slowest output is gobbled up by the pipe into subunit-trace.
    
    Change-Id: I4e23cb3c3773e58a297416c1f51996fef059330a

--- tools/pretty_tox.sh
+++ tools/pretty_tox.sh
@@ -7,5 +7,10 @@ set -o pipefail
 
 TESTRARGS=$1
 
-python setup.py testr --slowest --testr-args="--subunit $TESTRARGS" | subunit-trace -f
-
+python setup.py testr --testr-args="--subunit $TESTRARGS" | subunit-trace -f
+retval=$?
+# NOTE(mtreinish) The pipe above would eat the slowest display from pbr's testr
+# wrapper so just manually print the slowest tests
+echo -e "\nSlowest Tests:\n"
+testr slowest
+exit $retval
