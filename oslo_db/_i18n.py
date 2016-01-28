f289eb0d1dc549eb097a5b4527cd2b36f623efad
Akihiro Motoki <motoki@da.jp.nec.com>
2016-01-29 08:38:55 +0900

    Update translation setup
    Follow new infra setup for translations, see spec
    http://specs.openstack.org/openstack-infra/infra-specs/specs/translation_setup.html
    for full details.
    
    This basically renames oslo.db/locale/oslo.db.pot to
    oslo_db/locale/oslo_db.pot.
    For this we need to update setup.cfg.
    
    Update also domain name in _i18n.py.
    
    Change-Id: I5b3052f840201557f3b1c85effeda3bb3318a9ee

--- oslo_db/_i18n.py
+++ oslo_db/_i18n.py
@@ -19,7 +19,7 @@ See http://docs.openstack.org/developer/oslo.i18n/usage.html .
 import oslo_i18n
 
 
-_translators = oslo_i18n.TranslatorFactory(domain='oslo.db')
+_translators = oslo_i18n.TranslatorFactory(domain='oslo_db')
 
 # The primary translation function using the well-known name "_"
 _ = _translators.primary
