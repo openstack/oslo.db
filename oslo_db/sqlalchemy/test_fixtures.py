4522a9728ee9ba5e7c55faf392721c340eef9f55
Mike Bayer <mike_mp@zzzcomputing.com>
2016-07-15 16:31:09 -0400

    Break optimize_db_test_loader into package and module level
    This unittest-compatible load_tests function was not
    well organized for package-level tests and was broken
    for module-level usage; as in the case of module-level usage,
    the "pattern" parameter was being passed, which was unexpected
    since the Python 2.7 documentation for unittest claims it will
    be None.  However, we are using subunit to run tests, which
    uses the unittest2 library that achieves compatibility with
    Python 3 - in Python 3, the documentation was changed here
    to note that "pattern" may be present (and in fact it is).
    
    Instead, break it into optimize_package_test_loader and
    optimize_module_test_loader.  The former assumes placement
    inside of __init__.py only and the latter assumes placement
    inside of a specific module only.
    
    The issue with this function was observed when trying to get
    OptimisingTestSuite to work correctly with neutron; tests
    would be duplicated due to the double-search.  In local testing
    it was shown that the function could cause recursion overflows.
    
    It has been observed that using this function at the module level
    is of great use when running subsets of tests, since the package-level
    load_tests function is not consulted unless the test run is package-wide.
    
    Change-Id: I267fd2fbc16504c11addef870fc8d4fc500da7d9

--- oslo_db/sqlalchemy/test_fixtures.py
+++ oslo_db/sqlalchemy/test_fixtures.py
@@ -15,7 +15,9 @@
 
 import fixtures
 import logging
+import os
 import testresources
+import testscenarios
 
 from oslo_db import exception
 from oslo_db.sqlalchemy import enginefacade
@@ -544,3 +546,79 @@ class MySQLOpportunisticFixture(OpportunisticDbFixture):
 
 class PostgresqlOpportunisticFixture(OpportunisticDbFixture):
     DRIVER = 'postgresql'
+
+
+def optimize_package_test_loader(file_):
+    """Organize package-level tests into a testresources.OptimizingTestSuite.
+
+    This function provides a unittest-compatible load_tests hook
+    for a given package; for per-module, use the
+    :func:`.optimize_module_test_loader` function.
+
+    When a unitest or subunit style
+    test runner is used, the function will be called in order to
+    return a TestSuite containing the tests to run; this function
+    ensures that this suite is an OptimisingTestSuite, which will organize
+    the production of test resources across groups of tests at once.
+
+    The function is invoked as::
+
+        from oslo_db.sqlalchemy import test_base
+
+        load_tests = test_base.optimize_package_test_loader(__file__)
+
+    The loader *must* be present in the package level __init__.py.
+
+    The function also applies testscenarios expansion to all  test collections.
+    This so that an existing test suite that already needs to build
+    TestScenarios from a load_tests call can still have this take place when
+    replaced with this function.
+
+    """
+
+    this_dir = os.path.dirname(file_)
+
+    def load_tests(loader, found_tests, pattern):
+        result = testresources.OptimisingTestSuite()
+        result.addTests(found_tests)
+        pkg_tests = loader.discover(start_dir=this_dir, pattern=pattern)
+        result.addTests(testscenarios.generate_scenarios(pkg_tests))
+
+        return result
+    return load_tests
+
+
+def optimize_module_test_loader():
+    """Organize module-level tests into a testresources.OptimizingTestSuite.
+
+    This function provides a unittest-compatible load_tests hook
+    for a given module; for per-package, use the
+    :func:`.optimize_package_test_loader` function.
+
+    When a unitest or subunit style
+    test runner is used, the function will be called in order to
+    return a TestSuite containing the tests to run; this function
+    ensures that this suite is an OptimisingTestSuite, which will organize
+    the production of test resources across groups of tests at once.
+
+    The function is invoked as::
+
+        from oslo_db.sqlalchemy import test_base
+
+        load_tests = test_base.optimize_module_test_loader()
+
+    The loader *must* be present in an individual module, and *not* the
+    package level __init__.py.
+
+    The function also applies testscenarios expansion to all  test collections.
+    This so that an existing test suite that already needs to build
+    TestScenarios from a load_tests call can still have this take place when
+    replaced with this function.
+
+    """
+
+    def load_tests(loader, found_tests, pattern):
+        result = testresources.OptimisingTestSuite()
+        result.addTests(testscenarios.generate_scenarios(found_tests))
+        return result
+    return load_tests
