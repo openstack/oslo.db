944fe4b6c11abc06386eec670f10784277455b49
Vu Cong Tuan <tuanvc@vn.fujitsu.com>
2018-07-12 14:27:07 +0700

    Switch to stestr
    According to Openstack summit session [1],
    stestr is maintained project to which all Openstack projects should migrate.
    Let's switch to stestr as other projects have already moved to it.
    
    [1] https://etherpad.openstack.org/p/YVR-python-pti
    
    Change-Id: Ifd51e726de277d33ff61db47110bf8b5563fcda7

--- tools/run-pifpaf-tests.sh
+++ tools/run-pifpaf-tests.sh
@@ -3,5 +3,5 @@ set -e
 # Replace mysql:// by mysql+pymysql:// and add sqlite
 export OS_TEST_DBAPI_ADMIN_CONNECTION="${OS_TEST_DBAPI_ADMIN_CONNECTION/#mysql:/mysql+pymysql:};sqlite://"
 echo $OS_TEST_DBAPI_ADMIN_CONNECTION
-tools/pretty_tox.sh $*
-TEST_EVENTLET=1 tools/pretty_tox.sh $*
+stestr run $*
+TEST_EVENTLET=1 stestr run $*
