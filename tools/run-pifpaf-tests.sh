f048068216c655b6babc5740fc4b5e7efcf02ab1
Stephen Finucane <stephenfin@redhat.com>
2021-07-16 12:14:34 +0100

    tox: Simplify test running
    We were actually running tests four times. Twice with eventlet disabled
    and twice with it enabled. Correct this.
    
    Change-Id: Idb3926d73ff3c11abe9c273062aca1b299b9f72e
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- tools/run-pifpaf-tests.sh
+++ tools/run-pifpaf-tests.sh
@@ -3,5 +3,5 @@ set -e
 # Replace mysql:// by mysql+pymysql:// and add sqlite
 export OS_TEST_DBAPI_ADMIN_CONNECTION="${OS_TEST_DBAPI_ADMIN_CONNECTION/#mysql:/mysql+pymysql:};sqlite://"
 echo $OS_TEST_DBAPI_ADMIN_CONNECTION
-stestr run $*
-TEST_EVENTLET=1 stestr run $*
+TEST_EVENTLET=0 stestr run $*
+TEST_EVENTLET=1 stestr run --combine $*
