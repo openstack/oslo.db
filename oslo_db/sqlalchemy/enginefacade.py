a6d89f55e7b0ae298ee15b205f73587122e17821
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-03 16:29:14 +0900

    Remove deprecated async property
    This no longer works in Python 3.7 or later because async is now
    a keyword. Because now the minimum python version supported is 3.8,
    the compatibility code is not useful.
    
    See df6bf3401266f42271627c1e408f87c71a06cef7 for further details.
    
    Change-Id: Ib04858dda47fcbcafe027f222030506d7477e43b

--- oslo_db/sqlalchemy/enginefacade.py
+++ oslo_db/sqlalchemy/enginefacade.py
@@ -17,7 +17,6 @@ import operator
 import threading
 import warnings
 
-import debtcollector.moves
 import debtcollector.removals
 import debtcollector.renames
 from oslo_config import cfg
@@ -1053,18 +1052,6 @@ class _TransactionContextManager(object):
                 del transaction_contexts_by_thread.current
             elif current is not restore:
                 transaction_contexts_by_thread.current = restore
-
-
-@property
-@debtcollector.moves.moved_property("async_")
-def async_compat(self):
-    return self.async_
-
-
-setattr(
-    _TransactionContextManager,
-    "async", async_compat
-)
 
 
 def _context_descriptor(attr=None):
