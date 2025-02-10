f621db86dd77b61d4c07e0f91ff9063bc58b3848
Mike Bayer <mike_mp@zzzcomputing.com>
2025-02-10 13:12:24 -0500

    remove eventlet test dependency
    eventlet was only referenced within the test_async_eventlet
    suite, which would only run if TEST_EVENTLET were set.  I've
    confirmed that current zuul runs do not set this variable
    and these tests have likely been skipped for many years.
    
    As oslo.db has no other eventlet dependency this would make
    oslo.db fully compatible with full removal of eventlet from openstack.
    
    Change-Id: Icc33ce47f35b3e6fd813a265b7c337f37b18a57c

--- tools/run-pifpaf-tests.sh
+++ tools/run-pifpaf-tests.sh
@@ -3,5 +3,4 @@ set -e
 # Replace mysql:// by mysql+pymysql:// and add sqlite
 export OS_TEST_DBAPI_ADMIN_CONNECTION="${OS_TEST_DBAPI_ADMIN_CONNECTION/#mysql:/mysql+pymysql:};sqlite://"
 echo $OS_TEST_DBAPI_ADMIN_CONNECTION
-TEST_EVENTLET=0 stestr run $*
-TEST_EVENTLET=1 stestr run --combine $*
+stestr run $*
